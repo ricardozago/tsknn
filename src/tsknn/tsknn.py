dd29703bad0b4e2eba84e0a83cdbd6d0352b60f3
Ricardo Zago <ricardozago@users.noreply.github.com>
2024-07-21 15:04:58 -0300

    Adiciona kmeans na técnica

--- src/tsknn/tsknn.py
+++ src/tsknn/tsknn.py
@@ -23,7 +23,9 @@ class tsknn:
                  lags=3,
                  distance="euclidean",
                  h=12,
-                 msas="recursive"
+                 msas="recursive",
+                 kmeans = None,
+                 random_state = None,
                  ):
         self.k = k
         self.cf = cf.lower()
@@ -36,6 +38,9 @@ class tsknn:
             self.h_ef = 1
         else:
             self.h_ef = h
+        self.kmeans = kmeans
+        self.random_state = random_state
+
 
     def fit(self, X):
         if self.msas == 'mimo' and (self.h + np.max(self.lags) + self.k >= X.shape[0]):
@@ -44,14 +49,30 @@ class tsknn:
 
         self.windowed_arr = sliding_window_view(self.X[:-1], window_shape=(self.lags,), axis=0)
 
-        if self.transform == "multiplicative":
+        if self.transform == "multiplicative" and not self.kmeans:
             self.x_mean = self.windowed_arr.mean(axis=1)
             self.windowed_arr = self.windowed_arr / self.x_mean[:, np.newaxis]
-        elif self.transform == "additive":
+        elif self.transform == "additive" and not self.kmeans:
             self.x_mean = self.windowed_arr.mean(axis=1)
             self.windowed_arr = self.windowed_arr - self.x_mean[:, np.newaxis]
 
         self.windowed_arr = self.windowed_arr[:(1 - self.h_ef) if 1 - self.h_ef != 0 else None, :]
+
+        if self.kmeans:
+            from sklearn.cluster import KMeans
+            kmeans_model = KMeans(n_clusters=self.kmeans, random_state=self.random_state)
+            kmeans_model.fit(self.windowed_arr)
+            self.kmeans_means = np.array([np.mean(self.windowed_arr[kmeans_model.labels_ == i], axis=0) 
+                                    for i in range(self.kmeans)])
+            self.kmeans_labels = kmeans_model.labels_
+
+            if self.transform == "multiplicative":
+                self.x_mean = self.kmeans_means.mean(axis=1)
+                self.kmeans_means = self.kmeans_means / self.x_mean[:, np.newaxis]
+            elif self.transform == "additive":
+                self.x_mean = self.kmeans_means.mean(axis=1)
+                self.kmeans_means = self.kmeans_means - self.x_mean[:, np.newaxis]
+
 
     def _get_k_closest_positions(self, x_pred):
         '''
@@ -67,7 +88,10 @@ class tsknn:
 
         # rolled_result = np.apply_along_axis(lambda x: self.func_distance(x, x_pred), axis=-1, arr=self.windowed_arr)
         # rolled_result = np.sum((self.windowed_arr - x_pred)**2, axis=-1)
-        rolled_result = self.func_distance(self.windowed_arr, x_pred)
+        if self.kmeans:
+            rolled_result = self.func_distance(self.kmeans_means, x_pred)
+        else:
+            rolled_result = self.func_distance(self.windowed_arr, x_pred)
         index_closests = np.argpartition(rolled_result, range(self.k))[:self.k]
         distances = self.X.shape[0] - index_closests
         return index_closests, distances
@@ -76,6 +100,25 @@ class tsknn:
         '''
         Return the sequences to the knns
         '''
+        if self.kmeans:
+            resultado_final = np.zeros((len(k_closest), self.h_ef))
+            for j, cluster in enumerate(k_closest):
+                eqcluster = [index for index, value in enumerate(self.kmeans_labels == cluster) if value]
+
+                resultado = np.zeros((len(eqcluster), self.h_ef))
+                for i, pos in enumerate(eqcluster):
+                    if pos + self.h_ef <= self.X.shape[0]:
+                        resultado[i] = self.X[pos:pos + self.h_ef]
+                resultado = resultado.mean(0)
+                resultado_final[j] = resultado
+
+            if self.transform == "multiplicative":
+                return (resultado_final / self.x_mean[k_closest, np.newaxis]) * self.x_pred_mean
+            elif self.transform == "additive":
+                return (resultado_final - self.x_mean[k_closest, np.newaxis]) + self.x_pred_mean
+
+            return resultado_final
+
         k_closest = k_closest[:, np.newaxis] + np.tile(np.arange(self.h_ef), (len(k_closest), 1)) + self.lags
 
         if self.transform == "multiplicative":
@@ -105,7 +148,7 @@ class tsknn:
             raise ValueError('The biggest lag is different of the  length of example to predict')
         if self.msas == "recursive":
             y_preds = []
-            for i in range(self.h):
+            for _ in range(self.h):
                 index_closests, distances = self._get_k_closest_positions(X)
                 k_closest = self._get_k_closest(index_closests)
                 y_pred = self._get_mean(k_closest, distances)[0]
