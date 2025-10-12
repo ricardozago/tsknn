dcb7a6130f3a7939dfffde3291be1bab3dae4bbe
Ricardo Zago <ricardozago@users.noreply.github.com>
2025-10-12 15:07:45 -0300

    ajusta formatação com o black

--- tests/test_mimo.py
+++ tests/test_mimo.py
@@ -10,15 +10,23 @@ X = df.values.T[0]
 
 
 def test_answer():
-    model = tsknn(
-        cf="mean",
-        h=12,
-        transform="multiplicative",
-        lags=3,
-        msas="mimo")
+    model = tsknn(cf="mean", h=12, transform="multiplicative", lags=3, msas="mimo")
     model.fit(X)
     x_pred = model.predict()
-    resp = np.array([469.94095183, 496.33109352, 530.45167869, 541.48866369,
-                     530.14307769, 500.28989803, 525.41630756, 506.41834013,
-                     478.21661893, 475.52638617, 454.32574339, 484.28273386])
+    resp = np.array(
+        [
+            469.94095183,
+            496.33109352,
+            530.45167869,
+            541.48866369,
+            530.14307769,
+            500.28989803,
+            525.41630756,
+            506.41834013,
+            478.21661893,
+            475.52638617,
+            454.32574339,
+            484.28273386,
+        ]
+    )
     assert_almost_equal(x_pred, resp, decimal=5)
--- tsknn/tsknn.py
+++ tsknn/tsknn.py
@@ -18,30 +18,38 @@ class tsknn:
         random_state (int|None): Seed for reproducibility.
     """
 
-    def __init__(self,
-                 k=3,
-                 cf: str = "mean",
-                 transform: str = None,
-                 lags=3,
-                 distance: str = "euclidean",
-                 h: int = 12,
-                 msas: str = "recursive",
-                 kmeans: int = None,
-                 random_state: int = None,
-                 force_stable: bool = False
-                 ):
-        if not (isinstance(k, int) and k >= 1) and not (isinstance(
-                k, list) and all(isinstance(ki, int) and ki >= 1 for ki in k)):
+    def __init__(
+        self,
+        k=3,
+        cf: str = "mean",
+        transform: str = None,
+        lags=3,
+        distance: str = "euclidean",
+        h: int = 12,
+        msas: str = "recursive",
+        kmeans: int = None,
+        random_state: int = None,
+        force_stable: bool = False,
+    ):
+        if not (isinstance(k, int) and k >= 1) and not (
+            isinstance(k, list) and all(isinstance(ki, int) and ki >= 1 for ki in k)
+        ):
             raise ValueError(
-                "k must be a positive integer or a list of positive integers.")
+                "k must be a positive integer or a list of positive integers."
+            )
         if cf.lower() not in {"mean", "median", "weighted"}:
             raise ValueError("cf must be 'mean', 'median' or 'weighted'.")
         if transform is not None and transform.lower() not in {
-                "additive", "multiplicative"}:
+            "additive",
+            "multiplicative",
+        }:
             raise ValueError(
-                f"transform must be 'additive', 'multiplicative' or None, not {transform}")
-        if not (isinstance(lags, int) or (isinstance(lags, list)
-                and all(isinstance(lag, int) for lag in lags))):
+                f"transform must be 'additive', 'multiplicative' or None, not {transform}"
+            )
+        if not (
+            isinstance(lags, int)
+            or (isinstance(lags, list) and all(isinstance(lag, int) for lag in lags))
+        ):
             raise ValueError("lags must be an integer or a list of integers.")
         if not isinstance(h, int) or h < 1:
             raise ValueError("h must be a positive integer.")
@@ -76,10 +84,11 @@ class tsknn:
             raise TypeError("X must be a np.ndarray.")
         if X.ndim != 1:
             raise ValueError("X must be a one-dimensional array.")
-        if self.msas == 'mimo' and (
-                self.h + self.maxlags + np.max(self.k) >= X.shape[0]):
+        if self.msas == "mimo" and (
+            self.h + self.maxlags + np.max(self.k) >= X.shape[0]
+        ):
             raise ValueError(
-                'You need a bigger series, or change the mode to recursive'
+                "You need a bigger series, or change the mode to recursive"
             )
         self.X = X
 
@@ -88,8 +97,7 @@ class tsknn:
         )
 
         if isinstance(self.lags, list):
-            self.lag_indices = np.array(
-                [self.maxlags - lag for lag in self.lags])
+            self.lag_indices = np.array([self.maxlags - lag for lag in self.lags])
             self.windowed_arr = self.windowed_arr[:, self.lag_indices]
         else:
             self.lag_indices = None
@@ -102,11 +110,12 @@ class tsknn:
             self.windowed_arr = self.windowed_arr - self.x_mean[:, np.newaxis]
 
         self.windowed_arr = self.windowed_arr[
-            :(1 - self.h_ef) if 1 - self.h_ef != 0 else None, :
+            : (1 - self.h_ef) if 1 - self.h_ef != 0 else None, :
         ]
 
     def _get_k_closest_positions(
-            self, x_pred: np.ndarray, k: int = None) -> tuple[np.ndarray, np.ndarray]:
+        self, x_pred: np.ndarray, k: int = None
+    ) -> tuple[np.ndarray, np.ndarray]:
         """
         Returns the positions of the k nearest neighbors.
         Args:
@@ -137,10 +146,9 @@ class tsknn:
             index_closests = np.argsort(rolled_distances, stable=True)[:k]
         return index_closests, rolled_distances, x_pred_mean
 
-    def _get_k_closest(self,
-                       k_closest: np.ndarray,
-                       x_pred_mean: np.ndarray
-                       ) -> np.ndarray:
+    def _get_k_closest(
+        self, k_closest: np.ndarray, x_pred_mean: np.ndarray
+    ) -> np.ndarray:
         """
         Returns the sequences of the k nearest neighbors.
         Args:
@@ -156,13 +164,13 @@ class tsknn:
         )
 
         if self.transform == "multiplicative":
-            X = self.X[self.maxlags:]
+            X = self.X[self.maxlags :]
             return (
                 np.take(X, k_closest - self.maxlags)
                 / (self.x_mean[k_closest[:, 0] - self.maxlags, np.newaxis])
             ) * x_pred_mean
         elif self.transform == "additive":
-            X = self.X[self.maxlags:]
+            X = self.X[self.maxlags :]
             return (
                 np.take(X, k_closest - self.maxlags)
                 - (self.x_mean[k_closest[:, 0] - self.maxlags, np.newaxis])
@@ -174,7 +182,7 @@ class tsknn:
         self,
         k_closest: np.ndarray,
         index_closests: np.ndarray,
-        rolled_distances: np.ndarray
+        rolled_distances: np.ndarray,
     ) -> np.ndarray:
         """
         Returns the mean of the neighbors according to the combination function.
@@ -207,14 +215,15 @@ class tsknn:
             ValueError: If the size of X is not equal to the largest lag.
         """
         if X is None:
-            X = self.X[-self.maxlags:]
+            X = self.X[-self.maxlags :]
         if not isinstance(X, np.ndarray):
             raise TypeError("X must be a np.ndarray.")
         if X.ndim != 1:
             raise ValueError("X must be a one-dimensional array.")
         if X.shape[0] != self.maxlags:
             raise ValueError(
-                'The biggest lag is different from the length of the example to predict')
+                "The biggest lag is different from the length of the example to predict"
+            )
         k_list = self.k if isinstance(self.k, list) else [self.k]
         results = []
         for k in k_list:
@@ -222,22 +231,24 @@ class tsknn:
                 y_preds = []
                 X_temp = np.copy(X)
                 for _ in range(self.h):
-                    index_closests, rolled_distances, x_pred_mean = self._get_k_closest_positions(
-                        X_temp, k)
-                    k_closest = self._get_k_closest(
-                        index_closests, x_pred_mean)
+                    index_closests, rolled_distances, x_pred_mean = (
+                        self._get_k_closest_positions(X_temp, k)
+                    )
+                    k_closest = self._get_k_closest(index_closests, x_pred_mean)
                     y_pred = self._get_mean(
-                        k_closest, index_closests, rolled_distances)[0]
+                        k_closest, index_closests, rolled_distances
+                    )[0]
                     y_preds.append(y_pred)
-                    X_temp = np.concatenate(
-                        (X_temp, np.array([y_pred])), axis=0)[-self.maxlags:]
+                    X_temp = np.concatenate((X_temp, np.array([y_pred])), axis=0)[
+                        -self.maxlags :
+                    ]
                 y_preds = np.array(y_preds)
             elif self.msas == "mimo":
-                index_closests, rolled_distances, x_pred_mean = self._get_k_closest_positions(
-                    X, k)
+                index_closests, rolled_distances, x_pred_mean = (
+                    self._get_k_closest_positions(X, k)
+                )
                 k_closest = self._get_k_closest(index_closests, x_pred_mean)
-                y_preds = self._get_mean(
-                    k_closest, index_closests, rolled_distances)
+                y_preds = self._get_mean(k_closest, index_closests, rolled_distances)
             results.append(y_preds)
         if len(results) == 1:
             return results[0]
@@ -255,7 +266,7 @@ def sum_euclidean(M: np.ndarray, v: np.ndarray) -> np.ndarray:
     """
     # https://stackoverflow.com/a/49633639
     tmp = M - v
-    return np.einsum('ij,ij->i', tmp, tmp)
+    return np.einsum("ij,ij->i", tmp, tmp)
 
 
 def get_distance(distance: str = "euclidean"):
